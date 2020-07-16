acbb4b817b293610b5724e8808d330be2072beee
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-07-16 18:19:05 -0400

    tabs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonApp, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel, IonTabs } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { list, trophyOutline } from 'ionicons/icons';
 
 import CoursesPage from './pages/Courses';
 import CourseGoals from './pages/CourseGoals';
+import AllGoals from './pages/AllGoals';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -28,14 +30,30 @@ import './theme/variables.css';
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/" exact >
-          <CoursesPage />
-        </Route>
-        <Route path="/course-goals">
-          <CourseGoals />
-        </Route>
-      </IonRouterOutlet>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route path="/courses" exact >
+            <CoursesPage />
+          </Route>
+          <Route path="/all-goals">
+            <AllGoals />
+          </Route>
+          <Route path="/course-goals">
+            <CourseGoals />
+          </Route>
+          <Redirect to="/courses" />
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="all-goals" href="/all-goals">
+            <IonIcon icon={list} />
+            <IonLabel>All Goals</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="courses" href="/courses">
+            <IonIcon icon={trophyOutline} />
+            <IonLabel>Courses</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
     </IonReactRouter>
   </IonApp>
 );
---
+++ src/pages/AllGoals.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { IonHeader, IonContent, IonToolbar, IonTitle, IonPage, IonButtons, IonBackButton } from '@ionic/react';
+
+const AllGoals: React.FC = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar className="ion-text-center">
+                    <IonButtons slot="start">
+                        <IonBackButton defaultHref="/" />
+                    </IonButtons>
+                    <IonTitle>ALL Goals page</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonContent className="ion-text-center">
+                    <h3>ALL GOALS</h3>
+                </IonContent>
+            </IonContent>
+        </IonPage>
+    );
+}
+
+export default AllGoals;
