b444e0c80b4a23f9d0bd9760cc02c689b193dec0
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-07-16 19:35:56 -0400

    cards and display list added

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonHeader, IonContent, IonToolbar, IonTitle, IonButton, IonPage } from '@ionic/react';
+import { IonHeader, IonContent, IonToolbar, IonTitle, IonButton, IonPage, IonGrid, IonRow, IonCol, IonCard, IonCardContent } from '@ionic/react';
 // import { useHistory } from 'react-router-dom';
 
 const Courses: React.FC = () => {
@@ -9,20 +9,42 @@ const Courses: React.FC = () => {
     //     history.push('/course-goals');
     // };
 
+    const CORSE_DATA = [
+        { id: 'c1', title: 'IONIC course' },
+        { id: 'c2', title: 'This is another test title' },
+        { id: 'c3', title: 'Just a test data' },
+        { id: 'c4', title: 'IONIC with React' },
+        { id: 'c5', title: 'All this is just dummy data' },
+    ];
+
     return (
         <IonPage>
-        <IonHeader>
-            <IonToolbar className="ion-text-center">
-                <IonTitle>Courses page</IonTitle>
-            </IonToolbar>
-        </IonHeader>
-        <IonContent>
-            <IonContent className="ion-text-center">
-                <h3>it is working !</h3>
+            <IonHeader>
+                <IonToolbar className="ion-text-center">
+                    <IonTitle>Courses page</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonContent className="ion-text-center">
+                    {/* <h3>it is working !</h3>
                 <IonButton routerLink="/course-goals">To goals</IonButton>
-                {/* <IonButton onClick={changePageHandler} > To goals</IonButton> */}
+                <IonButton onClick={changePageHandler} > To goals</IonButton> */}
+                    <IonGrid>
+                        {CORSE_DATA.map(course => (
+                            <IonRow key={course.id}>
+                                <IonCol size-md="4" offset-md="4">
+                                    <IonCard>
+                                        <IonCardContent>
+                                            <h2>{course.title}</h2>
+                                            <IonButton size="small" shape="round" routerLink="">View Course Goals</IonButton>
+                                        </IonCardContent>
+                                    </IonCard>
+                                </IonCol>
+                            </IonRow>
+                        ))}
+                    </IonGrid>
+                </IonContent>
             </IonContent>
-        </IonContent>
         </IonPage>
     );
 };
