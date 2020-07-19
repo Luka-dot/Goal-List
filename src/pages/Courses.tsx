0d7c55db9b1e25a5fc6c0f73ed47437b330fbda4
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-07-19 12:38:34 -0400

    Add Course modal added and wired

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 import {
-  IonHeader,
-  IonContent,
-  IonToolbar,
-  IonTitle,
-  IonPage,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonCard,
-  IonCardContent,
-  IonButton,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardSubtitle,
-  IonFab,
-  IonFabButton,
-  IonIcon
+    IonHeader,
+    IonContent,
+    IonToolbar,
+    IonTitle,
+    IonPage,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCard,
+    IonCardContent,
+    IonButton,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonFab,
+    IonFabButton,
+    IonIcon
 } from '@ionic/react';
 import { addOutline } from 'ionicons/icons';
 // import { useHistory } from 'react-router-dom';
@@ -24,113 +24,113 @@ import { addOutline } from 'ionicons/icons';
 import AddCourseModal from '../components/CourseModal';
 
 export const COURSE_DATA = [
-  {
-    id: 'c1',
-    title: 'Ionic + React - The Practical Guide',
-    enrolled: new Date('03/22/2019'),
-    goals: [
-      { id: 'c1g1', text: 'Finish the course!  THIS IS SOOOO STUPID' },
-      { id: 'c1g2', text: 'Learn a lot!' },
-      { id: 'c1g3', text: 'Why is IONIC reload messed up???' },
-      { id: 'c1g4', text: 'More and MORE data!' }
-    ]
-  },
-  {
-    id: 'c2',
-    title: 'React.js - The Complete Guide',
-    enrolled: new Date('05/15/2018'),
-    goals: [
-      { id: 'c2g1', text: 'Finish the course!' },
-      { id: 'c2g2', text: 'Learn a lot!' },
-      { id: 'c2g3', text: 'Why is IONIC reload messed up???' },
-      { id: 'c2g4', text: 'More and MORE data!' }
-    ]
-  },
-  {
-    id: 'c3',
-    title: 'JavaScript - The Complete Guide',
-    enrolled: new Date('01/22/2020'),
-    goals: [
-      { id: 'c3g1', text: 'Finish the course!' },
-      { id: 'c3g2', text: 'Learn a lot!' },
-      { id: 'c3g3', text: 'Why is IONIC reload messed up???' },
-      { id: 'c3g4', text: 'More and MORE data!!!' }
-    ]
-  }
+    {
+        id: 'c1',
+        title: 'Ionic + React - The Practical Guide',
+        enrolled: new Date('03/22/2019'),
+        goals: [
+            { id: 'c1g1', text: 'Finish the course!  THIS IS SOOOO STUPID' },
+            { id: 'c1g2', text: 'Learn a lot!' },
+            { id: 'c1g3', text: 'Why is IONIC reload messed up???' },
+            { id: 'c1g4', text: 'More and MORE data!' }
+        ]
+    },
+    {
+        id: 'c2',
+        title: 'React.js - The Complete Guide',
+        enrolled: new Date('05/15/2018'),
+        goals: [
+            { id: 'c2g1', text: 'Finish the course!' },
+            { id: 'c2g2', text: 'Learn a lot!' },
+            { id: 'c2g3', text: 'Why is IONIC reload messed up???' },
+            { id: 'c2g4', text: 'More and MORE data!' }
+        ]
+    },
+    {
+        id: 'c3',
+        title: 'JavaScript - The Complete Guide',
+        enrolled: new Date('01/22/2020'),
+        goals: [
+            { id: 'c3g1', text: 'Finish the course!' },
+            { id: 'c3g2', text: 'Learn a lot!' },
+            { id: 'c3g3', text: 'Why is IONIC reload messed up???' },
+            { id: 'c3g4', text: 'More and MORE data!!!' }
+        ]
+    }
 ];
 
 const Courses: React.FC = () => {
-  // const history = useHistory();
+    // const history = useHistory();
 
-  // const changePageHandler = () => {
-  //   history.push('/course-goals');
-  // };
+    // const changePageHandler = () => {
+    //   history.push('/course-goals');
+    // };
 
-  const [ addingCourse, setAddingCourse ] = useState(false);
+    const [addingCourse, setAddingCourse] = useState(false);
 
-  const startAddCourseHandler = () => {
-    setAddingCourse(true);
-  };
+    const startAddCourseHandler = () => {
+        setAddingCourse(true);
+    };
 
-  const cancelAddCourse = () => {
-      setAddingCourse(false)
-  }
+    const cancelAddCourse = () => {
+        setAddingCourse(false)
+    }
 
-  return (
-      <React.Fragment>
-          <AddCourseModal
-        show={addingCourse}
-        onCancel={cancelAddCourse}
-      />
-    <IonPage>
-        
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Courses</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonGrid>
-          {COURSE_DATA.map(course => (
-            <IonRow key={course.id}>
-              <IonCol size-md="4" offset-md="4">
-                <IonCard>
-                  <IonCardHeader>
-                    <IonCardTitle>{course.title}</IonCardTitle>
-                    <IonCardSubtitle>
-                      Enrolled on{' '}
-                      {course.enrolled.toLocaleDateString('en-US', {
-                        year: 'numeric',
-                        month: '2-digit',
-                        day: '2-digit'
-                      })}
-                    </IonCardSubtitle>
-                  </IonCardHeader>
-                  <IonCardContent>
-                    <div className="ion-text-right">
-                      <IonButton
-                        fill="clear"
-                        color="secondary"
-                        routerLink={`/courses/${course.id}`}
-                      >
-                        View Course Goals
+    return (
+        <React.Fragment>
+            <AddCourseModal
+                show={addingCourse}
+                onCancel={cancelAddCourse}
+            />
+            <IonPage>
+
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Courses</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent>
+                    <IonGrid>
+                        {COURSE_DATA.map(course => (
+                            <IonRow key={course.id}>
+                                <IonCol size-md="4" offset-md="4">
+                                    <IonCard>
+                                        <IonCardHeader>
+                                            <IonCardTitle>{course.title}</IonCardTitle>
+                                            <IonCardSubtitle>
+                                                Enrolled on{' '}
+                                                {course.enrolled.toLocaleDateString('en-US', {
+                                                    year: 'numeric',
+                                                    month: '2-digit',
+                                                    day: '2-digit'
+                                                })}
+                                            </IonCardSubtitle>
+                                        </IonCardHeader>
+                                        <IonCardContent>
+                                            <div className="ion-text-right">
+                                                <IonButton
+                                                    fill="clear"
+                                                    color="secondary"
+                                                    routerLink={`/courses/${course.id}`}
+                                                >
+                                                    View Course Goals
                       </IonButton>
-                    </div>
-                  </IonCardContent>
-                </IonCard>
-              </IonCol>
-            </IonRow>
-          ))}
-        </IonGrid>
-      </IonContent>
-      <IonFab horizontal="end" vertical="bottom">
-              <IonFabButton color="secondary" >
-                <IonIcon icon={addOutline} onClick={startAddCourseHandler} />
-              </IonFabButton>
-            </IonFab>
-    </IonPage>
-    </React.Fragment>
-  );
+                                            </div>
+                                        </IonCardContent>
+                                    </IonCard>
+                                </IonCol>
+                            </IonRow>
+                        ))}
+                    </IonGrid>
+                </IonContent>
+                <IonFab horizontal="end" vertical="bottom">
+                    <IonFabButton color="secondary" >
+                        <IonIcon icon={addOutline} onClick={startAddCourseHandler} />
+                    </IonFabButton>
+                </IonFab>
+            </IonPage>
+        </React.Fragment>
+    );
 };
 
 export default Courses;
