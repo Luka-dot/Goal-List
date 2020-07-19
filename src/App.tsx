a56c1558409d9f344f702d6da5d8798c43072e29
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-07-19 18:42:42 -0400

    React context API added

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,8 @@ import { list, options } from 'ionicons/icons';
 
 import Filter from './pages/Filter';
 import CourseTabs from './pages/CourseTabs';
+import SideDrawer from './components/SideDrawer';
+import CoursesContextProvider from './data/CoursesContextProvider';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -40,33 +42,13 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 import './theme/theme.css';
 
+
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonMenu contentId="main">
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Course Goals</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent>
-          <IonList>
-            <IonMenuToggle>
-              <IonItem button routerLink="/courses/all-goals" routerDirection="none">
-                <IonIcon slot="start" icon={list} />
-                <IonLabel>All Goals</IonLabel>
-              </IonItem>
-            </IonMenuToggle>
-            <IonMenuToggle>
-              <IonItem button routerLink="/filter" routerDirection="none">
-                <IonIcon slot="start" icon={options} />
-                <IonLabel>Filter</IonLabel>
-              </IonItem>
-            </IonMenuToggle>
-          </IonList>
-        </IonContent>
-      </IonMenu>
-      <IonRouterOutlet id="main">
+      <SideDrawer />
+      <CoursesContextProvider>
+      <IonRouterOutlet id="main">  
         <Route path="/filter" exact>
           <Filter />
         </Route>
@@ -75,6 +57,7 @@ const App: React.FC = () => (
         </Route>
         <Redirect path='/' to='/courses' exact />
       </IonRouterOutlet>
+      </CoursesContextProvider>
     </IonReactRouter>
   </IonApp>
 );
---
+++ src/data/CoursesContextProvider.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import CoursesContext, { Course } from './courses-context';
+
+const CoursesContextProvider: React.FC = props => {
+    const [ courses, setCourses ] = useState<Course[]>([]);
+
+    const addCourse = () => {};
+
+    const addGoal = () => {}; 
+
+    const deleteGoal =() => {};
+
+    const updateGoal = () => {};
+
+    return (
+        <CoursesContext.Provider 
+            value={{
+                courses: courses, 
+                addGoal: addGoal, 
+                addCourse: addCourse, 
+                deleteGoal: deleteGoal, 
+                updateGoal: updateGoal 
+                }} 
+            >
+          {props.children}  
+        </CoursesContext.Provider>
+    );
+};
+
+export default CoursesContextProvider;
