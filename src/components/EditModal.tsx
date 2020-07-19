d00743a409f518e20ceb70346b705c0dc56116ab
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-07-19 10:13:52 -0400

    modal styling to rows and col

--- src/components/EditModal.tsx
+++ src/components/EditModal.tsx
@@ -1,27 +1,48 @@
 import React from 'react';
-import { 
-    IonModal, 
-    IonHeader, 
+import {
+    IonModal,
+    IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
-    IonButton 
+    IonButton,
+    IonGrid,
+    IonCol,
+    IonRow,
+    IonInput,
+    IonItem,
+    IonLabel
 } from '@ionic/react';
 
-const EditModal: React.FC<{show: boolean; onCancel: () => void }> = (props) => {
+const EditModal: React.FC<{ show: boolean; onCancel: () => void }> = (props) => {
     return (
         <IonModal isOpen={props.show}>
-                <IonHeader>
-                    <IonToolbar>
-                        <IonTitle>Edit goal</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonContent>
-                    <p>Editing...</p>
-                    <IonButton onClick={props.onCancel} >Cancel</IonButton>
-                    <IonButton>Save</IonButton>
-                </IonContent>
-            </IonModal>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Edit goal</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="floating" >Your Goal</IonLabel>
+                                <IonInput type="text" />
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow className="ion-text-center" >
+                        <IonCol>
+                            <IonButton color="dark" fill="clear" onClick={props.onCancel} >Cancel</IonButton>
+                        </IonCol>
+                        <IonCol>
+                            <IonButton color="secondary" expand="block" >Save</IonButton>
+                        </IonCol>   
+                    </IonRow>
+                </IonGrid> 
+            </IonContent>
+        </IonModal>
     );
 };
 
